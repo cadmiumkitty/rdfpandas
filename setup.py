f4931b9874492966ac0f4ff8cbd9548e7d7bf333
Eugene Morozov <emorozov@gmail.com>
2021-01-10 18:32:48 +0000

    Cleans up formatting in setup.py for #4.

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='rdfpandas',
-    version='1.1.0',
-    description='RDF support for Pandas',
-    long_description=readme,
-    author='Eugene Morozov',
-    author_email='emorozov@gmail.com',
-    url='https://github.com/cadmiumkitty/rdfpandas',
-    license='MIT',
-    packages=find_packages(exclude=('tests', 'docs')),
+    name = 'rdfpandas',
+    version = '1.1.0',
+    description = 'RDF support for Pandas',
+    long_description = readme,
+    author = 'Eugene Morozov',
+    author_email = 'emorozov@gmail.com',
+    url = 'https://github.com/cadmiumkitty/rdfpandas',
+    license = 'MIT',
+    packages = find_packages(exclude = ('tests', 'docs')),
     install_requires = ['pandas>=1.2.0', 'rdflib>=5.0.0']
 )
 
