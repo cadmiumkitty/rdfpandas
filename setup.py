e23d1d9a0a5400d0931a5db70802bb0ecd077a40
Eugene Morozov <emorozov@gmail.com>
2022-05-27 16:41:44 +1000

    Bumps up version to 1.1.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name = 'rdfpandas',
-    version = '1.1.2',
+    version = '1.1.3',
     description = 'RDF support for Pandas',
     long_description = readme,
     author = 'Eugene Morozov',
